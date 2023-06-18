fc1997c3c28b9cc5a65cd76c944f57cf8597ff5c
Olga Orlova <olga.orlova1987@gmail.com>
2023-06-18 23:37:41 +0300

    Сделала часть ДЗ 1 семинара

---
+++ Homework1/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+using System;
+
+Console.WriteLine("Введите первое число:");
+string? numberStringOne = Console.ReadLine();
+
+Console.WriteLine("Введите второе число:");
+string? numberStringTwo = Console.ReadLine();
+
+int numberIntOne = Convert.ToInt32(numberStringOne);
+int numberIntTwo = Convert.ToInt32(numberStringTwo);
+
+if(numberIntOne > numberIntTwo)
+{
+    Console.WriteLine(numberIntOne);
+    Console.WriteLine("Это число max");
+    Console.WriteLine(numberIntTwo);
+    Console.WriteLine("Это число min");
+}
+else
+{
+    Console.WriteLine(numberIntTwo);
+    Console.WriteLine("Это число max");
+    Console.WriteLine(numberIntOne);
+    Console.WriteLine("Это число min");
+}
---
+++ Homework1/Task4/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 4: Напишите программу, 
+// которая принимает на вход три числа и 
+// выдаёт максимальное из этих чисел.
+
+using System;
+
+Console.WriteLine("Введите первое число:");
+string? numberStringOne = Console.ReadLine();
+
+Console.WriteLine("Введите второе число:");
+string? numberStringTwo = Console.ReadLine();
+
+Console.WriteLine("Введите третье число:");
+string? numberStringThree = Console.ReadLine();
+
+
+int numberIntOne = Convert.ToInt32(numberStringOne);
+int numberIntTwo = Convert.ToInt32(numberStringTwo);
+int numberIntThree = Convert.ToInt32(numberIntThree);
+
+If(numberIntOne > numberIntTwo; numberIntOne > numberIntThree)
+{
+    Console.WriteLine ("Максимальное число:");
+    Console.WriteLine (numberIntOne);
+}
+if(numberIntTwo > numberIntThree; numberIntTwo > numberIntOne)
+{
+    Console.WriteLine ("Максимальное число:");
+    Console.WriteLine (numberIntTwo);
+}
+if(numberIntThree > numberIntOne; numberIntThree > numberIntTwo)
+{
+    Console.WriteLine ("Максимальное число:");
+    Console.WriteLine (numberIntThree);
+}
+
+
