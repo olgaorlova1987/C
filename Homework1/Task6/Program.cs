ac33069f828fbd7ad09dcd6fe2137de61712d583
Olga Orlova <olga.orlova1987@gmail.com>
2023-06-19 22:24:56 +0300

    Решила задачу 6 ДЗ первого семинара

---
+++ Homework1/Task6/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите число:");
+string? numberStringOne = Console.ReadLine();
+
+int numberIntOne = Convert.ToInt32(numberStringOne);
+if(numberIntOne % 2 ==0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число является нечетным");
+}
