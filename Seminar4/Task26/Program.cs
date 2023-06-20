f2dead77dee0ec72f3874f5b1d5689ac5d78ae4d
Olga Orlova <olga.orlova1987@gmail.com>
2023-06-20 22:36:08 +0300

    Решили задачи на 4 семинаре

---
+++ Seminar4/Task26/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 26: Напишите программу, которая принимает 
+//на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int ReadInt()
+{
+    Console.WriteLine("Введите целое число:");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+int func(int number)
+{
+    int colvo = 0;
+    while (number >0)
+    {
+        number = number/10;
+        colvo++;
+    }
+    return colvo;
+}
+int number = ReadInt();
+int colvo1 = func(number);
+
+Console.WriteLine($"Количество цифр: {colvo1}");
---
+++ Seminar4/Task28/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 28: Напишите программу, которая 
+//принимает на вход число N и выдаёт произведение 
+//чисел от 1 до N.
+// 4 -> 24 
+// 5 -> 120
+
+int ReadInt()
+{
+    Console.WriteLine("Введите число:");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+int func(int number)
+{
+    int colvo = 1;
+    while (number >0)
+    {
+        number = number * colvo;
+        colvo++;
+    }
+    return colvo;
+}
+int number = ReadInt();
+int colvo1 = func(number);
+
+Console.WriteLine($"Произведение чисел от 1 до N: {colvo1}");
