636f169302938dd0b68e55f02443e7a62c0b86d5
Olga Orlova <olga.orlova1987@gmail.com>
2023-06-22 13:09:26 +0300

    Решила последнюю 8 задачу 1 дз (с ошибкой)

--- Homework1/Task8/Program.cs
+++ Homework1/Task8/Program.cs
@@ -3,18 +3,11 @@
 // 8 -> 2, 4, 6, 8
 
 using System;
-
-Console.WriteLine ("Введите число N");
+Console.WriteLine("Введите число N");
 string? n = Console.ReadLine();
-int b = Convert.ToInt32(n);
-int i;
-for (int i => 1, i <= b; i++)
-{
-Console.WriteLine (i);
-Console.WriteLine("Все числа от 1 до N:");
-}
-
-for (i%2 == 0, i<= b; i++) 
+int b= Convert.ToInt32(n);
+int i= 1;
+for(i<= b; i%2 == 0; i++;)
 {
 Console.WriteLine(i);
 Console.WriteLine("Все четные числа от 1 до N");
